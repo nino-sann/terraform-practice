06aff0a54b20fe9864f2634c6bc098899cb0188f
nino-sann <yukkiness03@gmail.com>
2026-02-07 15:16:42 +0900

    terraform-plan.ymlを作成

--- terraform.tf
+++ terraform.tf
@@ -8,3 +8,11 @@ terraform {
 
   required_version = "= 1.14.3"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-githubactions-practice"
+    key    = "terraform.tfstate" # 保存するファイル名
+    region = "ap-northeast-1"
+  }
+}
